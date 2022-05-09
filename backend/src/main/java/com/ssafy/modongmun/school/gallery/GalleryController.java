4fca28374b7ff8532fd94105bd61c4178c872fc8
김나린 <knl_1219@hanmail.net>
2022-05-09 14:03:53 +0900

    feature: gallery 수정

--- backend/src/main/java/com/ssafy/modongmun/school/gallery/GalleryController.java
+++ backend/src/main/java/com/ssafy/modongmun/school/gallery/GalleryController.java
@@ -2,16 +2,11 @@ package com.ssafy.modongmun.school.gallery;
 
 import com.ssafy.modongmun.school.gallery.dto.GalleryPostDto;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -22,28 +17,7 @@ public class GalleryController {
     private final GalleryService galleryService;
 
     @PostMapping("/gallery/photos")
-    public ResponseEntity<?> postPhoto(GalleryPostDto galleryPostDto) throws IOException {
-        MultipartFile photo = galleryPostDto.getPhoto();
-        if(photo != null && !photo.isEmpty()){
-            System.out.println("file 확인");
-            String originName, saveFolder, imgPath;
-
-            saveFolder = "C:" + File.separator + "PJT" + File.separator + "test0506" + File.separator;
-            System.out.println("저장 경로 확인" + saveFolder);
-
-            File folder = new File(saveFolder);
-            if(!folder.exists()){
-                System.out.println("폴더 생성");
-                folder.mkdir();
-            }
-
-            originName = photo.getOriginalFilename();
-            if(originName != null) {
-                imgPath = saveFolder + originName;
-                photo.transferTo(new File(saveFolder, originName));
-                galleryPostDto.setImgPath(imgPath);
-            }
-        }
+    public ResponseEntity<?> postPhoto(@RequestBody GalleryPostDto galleryPostDto) throws IOException {
         galleryService.postPhoto(galleryPostDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -51,25 +25,12 @@ public class GalleryController {
     @GetMapping("/gallery/photos/{photo_id}")
     public ResponseEntity<GalleryPostDto> getPhoto(@PathVariable("photo_id") Long photoId) throws Exception{
         GalleryPostDto galleryPostDto = galleryService.getPhoto(photoId);
-
-        // 이미지 반환
-//        System.out.println(galleryPostDto.getImgPath());
-//        InputStream photoStream = new FileInputStream(galleryPostDto.getImgPath());
-//        galleryPostDto.setDetailPhoto(IOUtils.toByteArray(photoStream));
-
         return new ResponseEntity<>(galleryPostDto, HttpStatus.OK);
     }
 
     @GetMapping("/gallery/photos")
     public ResponseEntity<List<GalleryPostDto>> getPhotoList() throws Exception{
         List<GalleryPostDto> galleryPostDtoList = galleryService.getPhotoList();
-//        InputStream photoStream;
-//
-//        for(GalleryPostDto galleryPostDto : galleryPostDtoList){
-//            photoStream = new FileInputStream(galleryPostDto.getImgPath());
-//            galleryPostDto.setDetailPhoto(IOUtils.toByteArray(photoStream));
-//        }
-
         return new ResponseEntity<>(galleryPostDtoList, HttpStatus.OK);
     }
 
--- backend/src/main/java/com/ssafy/modongmun/school/gallery/GalleryService.java
+++ backend/src/main/java/com/ssafy/modongmun/school/gallery/GalleryService.java
@@ -6,9 +6,11 @@ import com.ssafy.modongmun.school.gallery.dto.GalleryPostDto;
 import com.ssafy.modongmun.user.User;
 import com.ssafy.modongmun.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +23,30 @@ public class GalleryService {
     private final SchoolRepository schoolRepository;
     private final UserRepository userRepository;
 
-    public void postPhoto(GalleryPostDto galleryPostDto) {
+    public void postPhoto(GalleryPostDto galleryPostDto) throws IOException {
+
+        MultipartFile photo = galleryPostDto.getPhoto();
+        if(photo != null && !photo.isEmpty()){
+            System.out.println("file 확인");
+            String originName, saveFolder, imgPath;
+
+            saveFolder = "C:" + File.separator + "PJT" + File.separator + "test0506" + File.separator;
+            System.out.println("저장 경로 확인" + saveFolder);
+
+            File folder = new File(saveFolder);
+            if(!folder.exists()){
+                System.out.println("폴더 생성");
+                folder.mkdir();
+            }
+
+            originName = photo.getOriginalFilename();
+            if(originName != null) {
+                imgPath = saveFolder + originName;
+                photo.transferTo(new File(saveFolder, originName));
+                galleryPostDto.setImgPath(imgPath);
+            }
+        }
+
         School school = schoolRepository.findById(galleryPostDto.getSchoolId()).orElse(null);
         User user = userRepository.findById(galleryPostDto.getUserId()).orElse(null);
 
@@ -34,9 +59,10 @@ public class GalleryService {
                 .build();
 
         galleryRepository.save(gallery);
+
     }
 
-    public GalleryPostDto getPhoto(Long photoId) {
+    public GalleryPostDto getPhoto(Long photoId) throws IOException {
 
         Gallery gallery = galleryRepository.findById(photoId).orElse(null);
         GalleryPostDto galleryPostDto = new GalleryPostDto();
@@ -44,13 +70,18 @@ public class GalleryService {
         galleryPostDto.setUserId(gallery.getUser().getUserId());
         galleryPostDto.setImgPath(gallery.getImgPath());
         galleryPostDto.setDescription(gallery.getDescription());
+        // InputStream photoStream = new FileInputStream(gallery.getImgPath());
+        // galleryPostDto.setDetailPhoto(IOUtils.toByteArray(photoStream));
+
         return galleryPostDto;
     }
 
-    public List<GalleryPostDto> getPhotoList() {
+    public List<GalleryPostDto> getPhotoList() throws IOException {
+
         List<Gallery> galleryList = galleryRepository.findAll();
-        System.out.println(galleryList);
         List<GalleryPostDto> galleryPostDtoList = new ArrayList<>();
+        // InputStream photoStream;
+
 
         for(Gallery gallery : galleryList){
             GalleryPostDto galleryPostDto = new GalleryPostDto();
@@ -58,6 +89,8 @@ public class GalleryService {
             galleryPostDto.setUserId(gallery.getUser().getUserId());
             galleryPostDto.setImgPath(gallery.getImgPath());
             galleryPostDto.setDescription(gallery.getDescription());
+            // photoStream = new FileInputStream(galleryPostDto.getImgPath());
+            // galleryPostDto.setDetailPhoto(IOUtils.toByteArray(photoStream));
             galleryPostDtoList.add(galleryPostDto);
         }
 
@@ -68,6 +101,5 @@ public class GalleryService {
         Gallery gallery = galleryRepository.findById(photoId).orElse(null);
         galleryRepository.delete(gallery);
     }
-
 
 }
