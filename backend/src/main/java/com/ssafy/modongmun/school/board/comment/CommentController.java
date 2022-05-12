601e4c6e46083c4929dd3e2b5456a0df5393d424
김나린 <knl_1219@hanmail.net>
2022-05-12 15:55:30 +0900

    Fix: CommentDto 수정

--- backend/src/main/java/com/ssafy/modongmun/school/board/comment/CommentController.java
+++ backend/src/main/java/com/ssafy/modongmun/school/board/comment/CommentController.java
@@ -1,7 +1,6 @@
 package com.ssafy.modongmun.school.board.comment;
 
 import com.ssafy.modongmun.school.board.comment.dto.CommentRegisterDto;
-import com.ssafy.modongmun.school.board.dto.BoardRegisterDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
