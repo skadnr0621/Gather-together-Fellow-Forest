b14b1dd41c8cd2abd90b624d60da99d3fb2db879
김나린 <knl_1219@hanmail.net>
2022-05-16 12:16:45 +0900

    Feat : [BE] searchSchool 기능 추가 - 동일 메서드가 중복 실행시 파라미터 에러 발생 - hibernate 5.6.8 버그로 스프링 부트 버전 2.5.4로 다운그레이드

--- backend/src/main/java/com/ssafy/modongmun/school/dto/SchoolDto.java
+++ backend/src/main/java/com/ssafy/modongmun/school/dto/SchoolDto.java
@@ -14,16 +14,7 @@ public class SchoolDto {
     private Long code;
     private String name;
     private String location;
-
-//    @Builder
-//    public SchoolDto(Long code, String name, String location) {
-////        this.SD_SCHUL_CODE = SD_SCHUL_CODE;
-////        this.SCHUL_NM = SCHUL_NM;
-////        this.LCTN_SC_NM = LCTN_SC_NM;
-//        this.code = code;
-//        this.name = name;
-//        this.location = location;
-//    }
+    private String keyword;
 
     public static SchoolDto toDto(School school) {
         return SchoolDto.builder()
