ae65c3a5633b32d4a970c82a29be65eedf1a4034
이호열 <xsrsx2000@gmail.com>
2022-05-19 04:35:15 +0900

    Chore: optimize imports

--- backend/src/main/java/com/ssafy/modongmun/school/gallery/GalleryService.java
+++ backend/src/main/java/com/ssafy/modongmun/school/gallery/GalleryService.java
@@ -8,12 +8,10 @@ import com.ssafy.modongmun.user.User;
 import com.ssafy.modongmun.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
