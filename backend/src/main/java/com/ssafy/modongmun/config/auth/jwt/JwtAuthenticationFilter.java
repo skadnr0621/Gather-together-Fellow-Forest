d550426f9e94eb0937e9dbee8185b79bf4565b9e
이호열 <xsrsx2000@gmail.com>
2022-05-19 16:55:03 +0900

    Chore: show exception stack trace

--- backend/src/main/java/com/ssafy/modongmun/config/auth/jwt/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/ssafy/modongmun/config/auth/jwt/JwtAuthenticationFilter.java
@@ -1,15 +1,12 @@
 package com.ssafy.modongmun.config.auth.jwt;
 
 import com.ssafy.modongmun.config.auth.UserPrincipalService;
-import com.ssafy.modongmun.user.User;
-import com.ssafy.modongmun.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -64,6 +61,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             }
             catch (Exception e) {
                 log.error("Authentication failed !!!");
+                e.printStackTrace();
             }
         }
         else { // empty Authentication Header or invalid prefix (not our token))
