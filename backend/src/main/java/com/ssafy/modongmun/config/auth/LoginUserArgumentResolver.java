31b0760143e57a6bcc602f253767ec7ac5020471
이호열 <xsrsx2000@gmail.com>
2022-05-19 16:48:34 +0900

    Feat: add @LoginUser functionality

---
+++ backend/src/main/java/com/ssafy/modongmun/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,29 @@
+package com.ssafy.modongmun.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserPrincipalClass = UserPrincipal.class.equals(parameter.getParameterType());
+        return isLoginUserAnnotation && isUserPrincipalClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getPrincipal();
+    }
+}
